810a184a1ef50fb0226c9ad8761b22897df5e4b8
Aynur Karimov <80378729+aynurkarimov@users.noreply.github.com>
2024-03-30 08:45:52 +0400

    Create file with given name

--- 03_fs/main.go
+++ 03_fs/main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
 )
 
 func main() {
-	selectFsAction()
+	fsAction := selectFsAction()
+
+	switch fsAction {
+	case "Create":
+		fileName := selectFileName()
+		createTextFile(fileName, ".txt")
+
+	default:
+		fileName := selectFileName()
+		createTextFile(fileName, ".txt")
+	}
 }
 
 var selectFsActionQuestion = &survey.Question{
@@ -32,4 +43,44 @@ func selectFsAction() string {
 	}
 
 	return fsActionAnswer
+}
+
+var fileNameQuestion = &survey.Question{
+	Name: "fileName",
+	Prompt: &survey.Input{
+		Message: "Name a file that should be created",
+	},
+	Validate:  survey.Required,
+	Transform: survey.ToLower,
+}
+
+func selectFileName() string {
+	var fileName string
+
+	err := survey.AskOne(fileNameQuestion.Prompt, &fileName)
+
+	if err != nil {
+		fmt.Println(err.Error())
+
+		os.Exit(0)
+	}
+
+	// Poor validation
+	fileNameWithoutExtension := strings.Split(fileName, ".")[0]
+
+	return fileNameWithoutExtension
+}
+
+func createTextFile(fileName string, extension string) error {
+	file, err := os.Create(fileName + extension)
+
+	if err != nil {
+		fmt.Println(err.Error())
+
+		os.Exit(0)
+	}
+
+	defer file.Close()
+
+	return nil
 }
